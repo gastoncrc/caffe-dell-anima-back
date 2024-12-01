8c75a623ffa79fd106970fe37f9e9bef74db90bf
Gaston <gastoncapdevila@hotmail.com>
2024-12-01 18:49:36 -0300

    config vercel

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import { Server } from "./models/server";
-import dotenv from "dotenv";
+// import { Server } from "./models/server";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
+
+// const server = new Server();
+// server.listen();
+
+import { VercelRequest, VercelResponse } from "@vercel/node"; // Importa los tipos de Vercel
+import { Server } from "./models/server";
 
 const server = new Server();
-server.listen();
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  // Pasa la solicitud y la respuesta a la instancia de Express
+  server.app(req, res);
+};
