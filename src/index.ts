16fe766a036c4fb708ae7a29c99c5c528558cbd9
Gaston <gastoncapdevila@hotmail.com>
2024-12-01 19:01:42 -0300

    modificacion del server

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,7 @@
-// import { Server } from "./models/server";
-// import dotenv from "dotenv";
-
-// dotenv.config();
-
-// const server = new Server();
-// server.listen();
-
-import { VercelRequest, VercelResponse } from "@vercel/node"; // Importa los tipos de Vercel
 import { Server } from "./models/server";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = new Server();
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  // Pasa la solicitud y la respuesta a la instancia de Express
-  server.app(req, res);
-};
+server.listen();
--- src/models/server.ts
+++ src/models/server.ts
@@ -1,51 +1,3 @@
-// import express, { Express } from "express";
-// import cors from "cors";
-// import connectDB from "../database/config";
-// import productRouter from "../routes/products";
-// import userRouter from "../routes/users";
-// import fileUpload from "express-fileupload";
-
-// export class Server {
-//   app: Express;
-//   port: string | number | undefined;
-
-//   constructor() {
-//     this.app = express();
-//     this.port = process.env.PORT;
-//     this.connectingDB();
-//     this.middlewares();
-//     this.routes();
-//   }
-
-//   async connectingDB(): Promise<void> {
-//     await connectDB();
-//   }
-
-//   middlewares(): void {
-//     this.app.use(express.json());
-//     this.app.use(
-//       fileUpload({
-//         useTempFiles: true,
-//         tempFileDir: "./uploads",
-//       })
-//     );
-//     this.app.use(cors());
-//   }
-
-//   routes(): void {
-//     this.app.use("/products", productRouter);
-//     this.app.use("/users", userRouter);
-//     this.app.use("/cart", userRouter);
-//   }
-
-//   listen(): void {
-//     this.app.listen(this.port, () => {
-//       console.log(`Server connected to port ${this.port}`);
-//     });
-//   }
-// }
-
-// models/server.ts
 import express, { Express } from "express";
 import cors from "cors";
 import connectDB from "../database/config";
@@ -84,5 +36,11 @@ export class Server {
     this.app.use("/products", productRouter);
     this.app.use("/users", userRouter);
     this.app.use("/cart", userRouter);
+  }
+
+  listen(): void {
+    this.app.listen(this.port, () => {
+      console.log(`Server connected to port ${this.port}`);
+    });
   }
 }
